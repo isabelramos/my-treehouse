e29b20ab6379d7468a94f3a7c571cdba8ade923a
Isabel Ramos <iramos1226@gmail.com>
2017-03-07 20:24:51 -0600

    profile thumbnail works

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+var treehouseContainer = document.getElementById("treehouse-container");
+
+function makeDom (xhrData) {
+	var treehouseString = "";
+	var currentObject;
+		currentObject = xhrData;
+
+		treehouseString += `<div class="col-sm-6 col-md-4">`;
+		treehouseString += `<div class="thumbnail">`;
+		treehouseString += `<img src="${currentObject.gravatar_url}" alt="treehouse-profile">`;
+		treehouseString += `<div class="caption">`;
+		treehouseString += `<h3>${currentObject.name}</h3>`;
+		treehouseString += `<p> Username: ${currentObject.profile_name}</p>`;
+		treehouseString += `<p> Points: ${currentObject.points.total}</p>`;		
+		treehouseString += `</div></div></div>`;
+	treehouseContainer.innerHTML = treehouseString;
+}
+
 function executeThisCodeAfterFileLoad () {
 	var data = JSON.parse(this.responseText);
 	console.log("woo!", data);
-	// makeDom(data);
+	makeDom(data);
 }
 
 function executeThisCodeAfterFileFail () {
